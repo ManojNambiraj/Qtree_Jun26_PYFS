1553f792d7de93630c42dddf60f8216a318d7a53
manojnambiraj <postboxmano@gmail.com>
2026-06-24 20:03:58 +0530

    JS Intro to Datatypes

---
+++ FrontEnd/JavaScript/Class1/script.js
@@ -0,0 +1,67 @@
+// Output Statements:
+
+    // alert("hi")
+    // console.log("hiiiiiiiii")
+    // DOM
+
+// JavaScript --> EcmaScript
+    // ES6
+    // ES5
+
+// Variables:
+
+    // my_age_in_my_school_days = 20
+
+    // console.log(my_age_in_my_school_days);
+
+// Hoisting: (variable declaration)
+
+    // var --> ES5
+
+        // var a = 10
+
+        // var a = 20
+
+        // console.log(a);
+
+    // let --> ES6
+
+        // let a = 10
+
+        // a = 20
+
+        // console.log(a);
+        
+    // const --> ES6
+
+        // const a = 10
+
+        // console.log(a);
+        
+// Scope: 
+
+    // 1. Global Scope --> Var
+    // 2. Local Scope --> Let & Const
+
+        // {
+        //     var a = 10
+
+        //     console.log("Inside:", a);
+            
+        // }
+
+        // console.log("Outside:", a);
+
+// Datatypes:
+
+    // let a = 100
+    // let a = 100.5
+    // let a = "Apple"
+    // let a = true
+    // let a
+    // const a = null
+
+    // console.log(a);
+    // console.log(typeof(a));
+
+
