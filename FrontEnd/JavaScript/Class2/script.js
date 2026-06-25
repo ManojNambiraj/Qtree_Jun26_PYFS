74d2409c0310a4357df4c732c19919c74a11aa43
manojnambiraj <postboxmano@gmail.com>
2026-06-25 19:57:12 +0530

    JS operators

---
+++ FrontEnd/JavaScript/Class2/script.js
@@ -0,0 +1,112 @@
+// Operators:
+
+    // 1. Arithmetic Operator
+        // (+, -, *, /, %, **, ++, --)
+
+            // let a = 5
+            // let b = 3
+            // let result
+
+            // // result = a + b
+            // // result = a - b
+            // // result = a * b
+            // // result = a / b
+            // // result = a % b
+            // // result = a ** b
+            // // result = ++a
+            // // result = --a
+
+            // console.log(result);
+
+    // 2. Assignment Operator:
+        // (=, +=, -=, *=, /=, %= **=)
+
+        // let a = 5
+
+        // a += 3    // a = a + 3
+        // a -= 3    // a = a - 3
+        // a *= 3    // a = a * 3
+
+        // console.log(a);
+
+    // 3. Comparision Operator: (Boolean)
+        // (==, ===, !=, !==, >, >=, <, <=)
+        
+            // let age = 18
+            // let result
+
+            // result = (age == 18)
+            // result = (age === 18)
+            // result = (age != 18)
+            // result = (age !== 18)
+            // result = (age >= 18)
+            // result = (age <= 18)
+
+            // console.log(result);
+    
+    // 4. Logical operator
+        // (&&, ||, !)
+
+        // &&
+
+        // (true)  && (true)  --> true
+        // (true)  && (false) --> false
+        // (false) && (true)  --> false
+        // (false) && (false) --> false
+
+        // ||
+
+        // (true)  || (true)  --> true
+        // (true)  || (false) --> true
+        // (false) || (true)  --> true
+        // (false) || (false) --> false
+
+        // !
+
+        // (true)  --> false
+        // (false) --> true
+
+            // let age = 21
+
+            // // result = (age > 18) && (age > 35)
+            // result = (age > 18) || (age > 35)
+
+            // console.log(!result);
+        
+    // 5. Bitwise operator:
+        // (&, |, ^, ~, <<, >>)
+
+        // // ^ --> EOR
+
+        //     // (1) ^ (1) --> 0
+        //     // (1) ^ (0) --> 1
+        //     // (0) ^ (1) --> 1
+        //     // (1) ^ (1) --> 0
+
+        // let a = 5 // 00000101
+        // let b = 2 // 00000010
+        // let result
+
+        // result = a >> 2
+
+        // console.log(result);
+        
+        // // Solution
+
+        // // (5) --> 00000101
+        // // -----------------
+        // // (1) --> 00000010
+        // // (2) --> 00000001
+
+    // 6. Ternary operator
+
+        // let age = 16
+
+        // let result = (age > 18) ? "Eligible for voting" : "Not Eligible"
+
+        // console.log(result);
+
+    // 7. Typeof Operator
+
+        // typeof(variable)
+    
