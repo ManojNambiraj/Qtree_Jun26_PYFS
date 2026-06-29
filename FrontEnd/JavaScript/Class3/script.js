66606675a1018e869a0c3544a24b887f868a5f89
manojnambiraj <postboxmano@gmail.com>
2026-06-29 19:59:54 +0530

    Upto Control statements

---
+++ FrontEnd/JavaScript/Class3/script.js
@@ -0,0 +1,114 @@
+// Control statements:
+    // 1. Conditional statements
+        // if
+
+            // let age = 21
+
+            // if(age > 18){
+            //     console.log("Eligible");
+            // }
+
+        // if else
+
+            // let age = 16;
+
+            // if (age > 18) {
+            //   console.log("Eligible");
+            // }else{
+            //     console.log("Not Eligible");
+            // }
+
+        // if else if
+
+            // let age = 18;
+
+            // if (age > 18) {
+            //     console.log("Eligible");
+            // } else if(age == 18){
+            //     console.log("Partially Eligible");
+            // } else if(age < 18) {
+            //     console.log("Not Eligible");
+            // }
+
+        // switch case
+
+        // let day = "Jan 1"
+
+        // switch (day) {
+        //     case "Mon":
+        //         console.log("It's a Monday");
+        //         break;
+        //     case 2:
+        //         console.log("It's a Tuesday");
+        //         break;
+        //     case 3:
+        //         console.log("It's a Wednesday");
+        //         break;
+        //     case 4:
+        //         console.log("It's a Thursday");
+        //         break;
+        //     case 5:
+        //         console.log("It's a Friday");
+        //         break;
+        //     case 6:
+        //         console.log("It's a Saturday");
+        //         break;
+        //     default:
+        //         console.log("Happy weekend");
+        // }
+
+
+    // 2. Looping statements
+        // for loop
+
+            // for(let i = 10; i >= 1; i--){
+            //     console.log(i);
+            // }
+
+        // while loop
+
+            // let i = 1;
+
+            // while(i <= 10){
+            //     console.log(i);
+            //     i++
+            // }
+
+        // do..while loop
+
+            // let i = 1
+
+            // do{
+            //     console.log(i);
+            //     i++;
+            // }while(i <= 10)
+
+    // 3. Jumping statements
+        // break
+
+            // let i = 1;
+
+            // while(i <= 5){
+
+            //     if(i == 3){
+            //         break
+            //     }
+
+            //     console.log(i);
+            //     i++
+            // }
+
+        // continue
+
+            // let i = 0;
+
+            // while (i < 5) {
+            //     i++;
+
+            //     if(i == 3){
+            //         continue
+            //     }
+
+            //     console.log(i);
+                
+            // }
