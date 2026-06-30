738d9dd7e2fee1401184a75d4efa59b7a7985be3
manojnambiraj <postboxmano@gmail.com>
2026-06-30 20:13:27 +0530

    JS Arrays & Objects

---
+++ FrontEnd/JavaScript/Class4/script.js
@@ -0,0 +1,89 @@
+// Functions:
+    // 1. Built-in functions
+    // 2. User defined functions
+
+
+    // DRY
+    
+// 1. Normal function:
+
+// function demo(x){          // Definition
+//     console.log(x);
+// }
+    
+// demo("hi");                    // call
+// demo("hello")                    // call
+// demo("hey")                    // call
+// demo(100)                    // call
+// demo()                    // call
+
+
+    // function mul(a, b){
+    //     return a * b    
+    // }
+
+    // let result = mul(2, 5)
+
+    // console.log(result);
+
+// 2. Ananymous Function
+
+    // let demo = function() {
+    //   return "hello world"
+    // };
+
+    // console.log(demo());
+
+// 3. Arrow function
+
+    // const arr = (a, b) => a * b;
+
+    // console.log(arr(2, 6));
+
+// 4. IIFE Function (Immediately Invoked Function Experssion)
+
+    // (() => {
+    //     console.log("Hello");
+    // })()
+
+
+// Array:
+
+    // let arr = [10, 20, 30, 40, 50]
+    // // index   #0  #1  #2...............
+
+
+    // console.log(arr);
+    // console.log(arr.length);
+
+    // console.log(arr[0] * arr[1]);
+
+    // arr[0] = 1000
+
+    // console.log(arr);
+
+    // arr.push(300)
+    // arr.pop()
+
+    // console.log(arr);
+
+// Objects:
+
+let student1 = {
+    name: "Sam",
+    age: 20,
+    dept: "IT",
+    mobile: "9876521213"
+}
+
+console.log(student1);
+console.log(student1.name);
+
+// student1.lastname = "Sundar"
+
+console.log(student1);
+
+
+
+
+
