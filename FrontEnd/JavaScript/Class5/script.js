5ea4b6ec12c92cd35f095d54a38d7e40aa76397f
manojnambiraj <postboxmano@gmail.com>
2026-07-01 20:08:41 +0530

    JS Arrays and Objects and Methods

---
+++ FrontEnd/JavaScript/Class5/script.js
@@ -0,0 +1,99 @@
+// Arrays:
+
+// ES6:
+
+    // Destructuring
+        // Array Destructuring
+
+            // let fruits = ["apple", "orange", "banana", "grapes", "kiwi"];
+            // //             #0         #1        #2..........
+
+            // console.log(fruits);
+
+            // const [red, oran, yellow, violet, green] = fruits;
+
+            // console.log(red);
+            // console.log(yellow);
+
+        // Object Destructuring
+
+        //     let student = {
+        //         name: "Ajay",
+        //         age: 21,
+        //         dept: "IT",
+        //         mobile: 987654321,
+        //         isMarried: false,
+        //         favColor: ["blue", "red", "black"],
+        //         address: {
+        //             "d.no": "No.2",
+        //             street: "Periyar Street",
+        //             landmark: "Water tank"
+        //         },
+        //         demo: function(){
+        //             console.log("Student DB");
+        //         }
+        //     }
+
+        //     // console.log(student);
+
+        //     // student["name"] = "Kumar"
+
+        //     // console.log(student);
+
+        //     // console.log(student.favColor[0]);
+        //     // console.log(student.address.street);
+        //     // student.demo()
+
+        //     // console.log(Object.keys(student));
+        //     // console.log(Object.values(student));
+            
+
+        //     // const {mobile, name, address} = student
+
+        //     // console.log(name);
+
+        //     // console.log(mobile);
+        
+        // // Spread or Rest Operator
+
+            //     // let x = [30, 10, 20]
+            //     // let y = [40, 50, 60]
+
+            //     // x = [...x, ...y]
+
+            //     // console.log(x);
+            //     // console.log(x.sort());
+        
+
+// Array Methods: (MRF methods)
+
+        // for(let i = 0; i < arr.length; i++){
+            //     console.log(arr[i]);
+        // }
+
+    // let arr = [2, 7, 9, 12, 14, 22, 77];
+
+
+    // Map
+
+        // let result = arr.map((item) => item * 2);
+
+        // console.log(result);
+
+    // Reduce
+
+        // let result = arr.reduce((acc, item) => acc + item);
+
+        // console.log(result);
+        
+    // Filter
+
+        // let result = arr.filter((item) => {
+        //   return item % 2 != 0;
+        // });
+
+        // console.log(result);
+
+  
+    
+
